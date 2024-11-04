8fc2e48b67d01b8c44dff7516d19ea96e50da393
Afonso Sousa <a104262@alunos.uminho.pt>
2024-11-04 12:09:04 +0000

    +

--- DSS/Ficha5/IGestTurmas.java
+++ DSS/Ficha5/IGestTurmas.java
@@ -1,6 +1,6 @@
 public interface IGestTurmas {
     public Aluno getAluno ( String codAluno );
-    float getNotaAluno ( String codAluno );
+    public int getNotaAluno(String codAluno);
     void registaEntrega ( Entrega e , String codGrupo );
     boolean validaAvaliadores ();
 }
